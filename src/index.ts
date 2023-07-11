febd1cb309dc64ef5765364da288a04b6b16ba9e
maanasmahato4 <maanasmahato456@gmail.com>
2023-07-11 21:21:13 +0545

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 export class Log {
-    static success(msg: string){
-        console.log(`%c ${msg}`, `color: green`);
+    static success(msg: string) {
+        console.log(`%c ${msg}`, `color:green`);
     }
-    static danger(msg: string){
-        console.log(`%c ${msg}`, `color:red`)
+    static danger(msg: string) {
+        console.log(`%c ${msg}`, `color:red`);
     }
-    static info(msg: string){
-        console.log(`%c ${msg}`, `color: blue`)
+    static info(msg: string) {
+        console.log(`%c ${msg}`, `color: blue`);
     }
-    static warning(msg: string){
-        console.log(`%c ${msg}`, `color: orange; background: black`)
+    static warning(msg: string) {
+        console.log(`%c ${msg}`, `color: orange`);
+    }
+    static light(msg: string) {
+        console.log(`%c ${msg}`, `color: white; background: black`);
+    }
+    static dark(msg: string) {
+        console.log(`%c ${msg}`, `color: black; background: white`);
     }
 }
